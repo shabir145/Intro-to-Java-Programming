0ddfa974f71fd6f95d45b086060ccbae44877335
shabir basir <basiraziz03@gmail.com>
2024-12-23 23:22:28 +0100

    comment added

--- Exercise_01/Exercise_01_01/Exercise_01_01.java
+++ Exercise_01/Exercise_01_01/Exercise_01_01.java
@@ -1,6 +1,6 @@
 /* (Display three messages) Write a program that displays Welcome to Java,
    Welcome to Computer Science, and Programming is fun.
-   */
+ this is another line to the comments of the code  */
 public class Exercise_01_01 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
